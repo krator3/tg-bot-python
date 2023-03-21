be0262c45bd8d20323190307d9ae151c2a925354
Maxim Ostapenko <128240873+krator3@users.noreply.github.com>
2023-03-21 21:59:40 +0300

    bot version 1

---
+++ bot_for_server.py
@@ -0,0 +1,49 @@
+from config import TOKEN
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from asyncio import sleep
+
+proxy_url = 'http://proxy.server:3128'
+
+bot = Bot(token=TOKEN,proxy=proxy_url)
+dp = Dispatcher(bot)
+
+button_linux = KeyboardButton("🐧Linux🐧")
+button_cube = KeyboardButton("🎲Бросить кубик🎲")
+kb_bot = ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True)
+kb_bot.add(button_linux, button_cube)
+
+@dp.message_handler(commands=["start"])
+async def cmd_start(msg: types.Message):
+    await msg.answer("Добро пожаловать!", reply_markup=kb_bot)
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    if msg.text == "🐧Linux🐧":
+        await msg.answer("🐧")
+    elif msg.text == ("🎲Бросить кубик🎲"):
+        await msg.answer("Игра началась!")
+        await sleep(4)
+        await msg.answer("Куб бота:")
+        bot_cube = await msg.answer_dice()
+        bot_cube = bot_cube["dice"]["value"]
+        await sleep(5)
+        await msg.answer("Ваш куб:")
+        user_cube = await msg.answer_dice()
+        user_cube = user_cube["dice"]["value"]
+        await sleep(5)
+        if bot_cube>user_cube:
+            await msg.answer("Вы проиграли!")
+        if bot_cube<user_cube:
+            await msg.answer("Вы победили!")
+        if bot_cube==user_cube:
+            await msg.answer("Ничья!")
+
+    else:
+        await bot.send_message(msg.from_user.id, msg.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
